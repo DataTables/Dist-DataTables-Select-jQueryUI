9d3cffe136205894f0d9514459f11ad2bc0fa703
Allan Jardine <allan@datatables.net>
2022-10-10 19:51:42 +0000

    Add support for mjs files (ES Modules)

---
+++ js/select.jqueryui.min.mjs
@@ -0,0 +1,1 @@
+import $ from"jquery";import DataTable from"datatables.net-ju";import DataTable from"datatables.net-select";export default DataTable;
---
+++ js/select.jqueryui.mjs
@@ -0,0 +1,13 @@
+
+/*! jQuery UI styling wrapper for Select
+ * © SpryMedia Ltd - datatables.net/license
+ */
+
+import $ from 'jquery';
+import DataTable from 'datatables.net-ju';
+import DataTable from 'datatables.net-select';
+
+
+
+
+export default DataTable;
