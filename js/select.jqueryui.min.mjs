e95e6f7b263f7093a291f05baa6837741d9457b8
Allan Jardine <allan@datatables.net>
2022-11-04 14:54:54 +0000

    Missing files in DataTables-DataTables

--- js/select.jqueryui.min.mjs
+++ js/select.jqueryui.min.mjs
@@ -1,4 +1,1 @@
-/*! jQuery UI styling wrapper for Select
- * © SpryMedia Ltd - datatables.net/license
- */
 import $ from"jquery";import DataTable from"datatables.net-ju";import DataTable from"datatables.net-select";export default DataTable;
