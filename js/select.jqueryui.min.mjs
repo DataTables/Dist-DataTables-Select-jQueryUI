c81071081dc0c04e9b8c26e741f9a7064ef5d25f
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-04 12:16:03 +0000

    db0816c431dce1ec8a32eecbb0ed0b8f52595f3e
    Sync to source repo @db0816c431dce1ec8a32eecbb0ed0b8f52595f3e

--- js/select.jqueryui.min.mjs
+++ js/select.jqueryui.min.mjs
@@ -1,1 +1,4 @@
+/*! jQuery UI styling wrapper for Select
+ * © SpryMedia Ltd - datatables.net/license
+ */
 import $ from"jquery";import DataTable from"datatables.net-ju";import DataTable from"datatables.net-select";export default DataTable;
