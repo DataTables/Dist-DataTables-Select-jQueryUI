02ae90ce98e15e3a2dc1ad198f352fbdf1dc36bb
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-16 17:08:44 +0000

    40e2cdadfd7d2e238c63cd85b4f7362e94b1d025
    Sync to source repo @0e2cdadfd7d2e238c63cd85b4f7362e94b1d025

--- js/select.jqueryui.js
+++ js/select.jqueryui.js
@@ -5,7 +5,7 @@
 (function( factory ){
 	if ( typeof define === 'function' && define.amd ) {
 		// AMD
-		define( ['jquery', 'datatables.net-ju', 'datatables.net-select'], function ( $ ) {
+		define( ['jquery', 'datatables.net-jqui', 'datatables.net-select'], function ( $ ) {
 			return factory( $, window, document );
 		} );
 	}
@@ -25,13 +25,12 @@
 			}
 
 			if ( ! $.fn.dataTable ) {
-				require('datatables.net-ju')(root, $);
+				require('datatables.net-jqui')(root, $);
 			}
 
-			if ( ! $.fn.dataTable ) {
+			if ( ! $.fn.dataTable.select ) {
 				require('datatables.net-select')(root, $);
 			}
-
 
 			return factory( $, root, root.document );
 		};
--- js/select.jqueryui.mjs
+++ js/select.jqueryui.mjs
@@ -3,8 +3,8 @@
  */
 
 import $ from 'jquery';
-import DataTable from 'datatables.net-ju';
-import 'datatables.net-select';
+import DataTable from 'datatables.net-jqui';
+import select from 'datatables.net-select';
 
 
 
